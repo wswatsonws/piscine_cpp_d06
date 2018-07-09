26daa36f947d243bc91b193974235a4ac9bd69db
Watson <you@example.com>
2018-07-09 23:58:53 +0800

    22:11

--- ex02/Squad.cpp
+++ ex02/Squad.cpp
@@ -6,7 +6,7 @@ Squad::Squad() : _vector()
 
 Squad::~Squad()
 {
-    for (std::vector<ISpaceMarine*>::iterator itr = _vector.begin(); itr != _vector.end(); ++itr)
+    for (std::vector<ISpaceMarine*>::iterator itr = _vector.begin(); itr != _vector.end(); itr++)
         delete *itr;
 }
 
@@ -24,7 +24,7 @@ int Squad::push(ISpaceMarine* unit)
 {
     if (!unit)
         return getCount();
-    for (std::vector<ISpaceMarine*>::const_iterator itr = _vector.begin(); itr != _vector.end(); ++itr)
+    for (std::vector<ISpaceMarine*>::const_iterator itr = _vector.begin(); itr != _vector.end(); itr++)
         if (unit == *itr)
             return getCount();
     _vector.push_back(unit);
